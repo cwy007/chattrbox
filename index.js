0d87fdaf1c726d16c7fefb11e0808aa63678434d
chenweiyan <chanweiyan007@gmail.com>
2020-06-26 23:12:39 +0800

    extractFilePath

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var http = require('http');
 var fs = require('fs');
-var path = require('path');
+var extract = require('./extract');
 
 var server = http.createServer(function (req, res) {
   console.log('Responding to a request.');
-  var url = req.url;
 
-  var fileName = 'index.html';
-  if (url.length > 1) {
-    fileName = url.substring(1);
-  }
-  console.log(fileName);
-  var filePath = path.resolve(__dirname, 'app', fileName);
+  var filePath = extract(req.url);
   fs.readFile(filePath, function (err, data) {
     res.end(data);
   });
