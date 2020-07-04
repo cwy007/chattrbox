c0e8f1d601254c84f1ee4146a5dfdf2e9ca770cf
chenweiyan <chanweiyan007@gmail.com>
2020-07-04 10:09:29 +0800

    destructuring assignment syntax

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -4,4 +4,24 @@ class ChatApp {
   }
 }
 
+class ChatMessage {
+  constructor({
+    message: m,
+    user: u='batman',
+    timestamp: t=(new Date()).getTime()
+  }) {
+    this.message = m;
+    this.user = u;
+    this.timestamp = t;
+  }
+
+  serialize() {
+    return {
+      user: this.user,
+      message: this.message,
+      timestamp: this.timestamp
+    }
+  };
+}
+
 export default ChatApp;
